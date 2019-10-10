92d52b4397caf6cd8fd44cf69cba183dd900f10b
wisleysouza <wisleysouza@gmail.com>
2019-10-10 15:01:58 +0200

    adding category model

---
+++ src/app/pages/categories/shared/category-model.ts
@@ -0,0 +1,8 @@
+import { CategoryFormComponent } from './../category-form/category-form.component';
+export class Category{
+    constructor(       
+        public id?:number,      
+        public name?:string,
+        public description?:string        
+    ){}
+}
